c4e452486116ab04aecb8b9b5cd8853d39531cde
AndrewGutierrezCastro <andrewgc@estudiantec.cr>
2020-10-29 00:48:56 -0600

    Update proyecto1.hs

--- proyecto1.hs
+++ proyecto1.hs
@@ -4,7 +4,9 @@ type Line = [Token]
 type HypMap = Data.Map.Map String [String]
 data Token = Word String | Blank | HypWord String 
                 deriving (Eq, Show)
-
+data Separacion = SEPARAR | NOSEPARAR
+data Ajuste = AJUSTAR | NOAJUSTAR
+     
 main :: IO ()
 
 string2Line :: String -> Line
@@ -25,21 +27,26 @@ enHyp = Data.Map.fromList [ ("controla",["con","tro","la"]),
 
 main = putStrLn "Hello, World!"
 
+--String2Line esta funcion convierte una tira en words
+-- >>string2Line "hola que tal"
+-- [Word "hola",Word "que",Word "tal"]
 string2Line str =  map string2Token (words str)
 
 string2Token str = Word str
 
-line2String line = unwords (map token2String line)
+--Line2String convierte un Line en una tira string
+--en medio de cada token agrega un espacio
+line2String line = concat (map token2String line)
 
 token2String (Word wrd) = wrd
 token2String (Blank) = " "
 token2String (HypWord hpw) = hpw++"-" 
 
-lineLength line = sum (map tokenLength line)
+lineLength line = sum (map tokenLength line) + ((length line)-1)
 
 tokenLength (Word wrd) = length wrd
 tokenLength (Blank) = 1
-tokenLength (HypWord hypWrd) = length hypWrd
+tokenLength (HypWord hypWrd) = length hypWrd + 1
 
 breakLine n (l:lines) = if largo <= n 
                             then (l : fst cumplen, snd cumplen )
@@ -102,7 +109,7 @@ insertBlanks n line
           numEspacios =  ((length line) - 1 )
 
 insertBlanksAux line listBlank
-    | line == [] || listBlank == [] = []
+    | line == [] || listBlank == [] = line
     | otherwise = (head line):(head listBlank) ++ insertBlanksAux (tail line) (tail listBlank)
 
 --elem : elemento a repetir
@@ -115,16 +122,42 @@ repElemt elem n m list
     | otherwise = list
     
 
-separarYalinear n separar ajuste tira
-    | separar == "NOSEPARAR" && ajuste == "NOAJUSTAR" = pegadoSinAjuste n tira
-    | separar == "NOSEPARAR" && ajuste == "AJUSTAR" = pegadoConAjuste n tira
-    | separar == "SEPARAR" && ajuste == "NOAJUSTAR" = separadoSinAjuste n tira
-    | separar == "SEPARAR" && ajuste == "AJUSTAR" = separadoConAjuste n tira
 
-pegadoSinAjuste n tira = tira
+separarYalinear n NOSEPARAR NOAJUSTAR tira = map line2String (pegadoSinAjuste n (string2Line tira) [] [])
+separarYalinear n NOSEPARAR AJUSTAR tira = map line2String (pegadoConAjuste n (string2Line tira) [] [])
+separarYalinear n SEPARAR NOAJUSTAR tira = separadoSinAjuste n (string2Line tira) []
+separarYalinear n SEPARAR AJUSTAR tira = separadoConAjuste n (string2Line tira) []
 
-pegadoConAjuste n tira = tira
 
-separadoSinAjuste n tira = tira
+--Esta funcion resive el n de largo maximo, una lista de tokens una lista 
+--final y una lista acumulada para ir creando cada linea
+pegadoSinAjuste n (x:xs) lstFinal lstAcum
+    | largoLstAcm <= n = pegadoSinAjuste n xs lstFinal (lstAcum++[x])
+    | largoLstAcm > n = pegadoSinAjuste n xs (lstFinal++[lstAcum]) [x]
+    
+    where largoLstAcm = ((lineLength lstAcum )+(tokenLength x)+((length lstAcum)-1) )
+    --el largo de la lista acumulada + el largo del siguiente token
 
-separadoConAjuste n tira = tira
+--Condicion de parada
+pegadoSinAjuste _ [] lstFinal lstAcum = lstFinal++[lstAcum]
+
+--Esta funcion
+--
+pegadoConAjuste n (x:xs) lstFinal lstAcum
+    | largoLstAcm < n = pegadoConAjuste n xs lstFinal (lstAcum++[x])
+    | largoLstAcm > n = pegadoConAjuste n xs (lstFinal++[(insertBlanks (n-largoLst+numEspacios) lstAcum)]) [x]
+    | largoLstAcm == n = pegadoConAjuste n xs (lstFinal++[(lstAcum++[x])]) []
+    
+    where largoLstAcm = (largoLst +(tokenLength x)) 
+          largoLst = lineLength lstAcum
+          numEspacios = (length lstAcum)-1
+    --el largo de la lista acumulada + el largo del siguiente token 
+pegadoConAjuste n [] lstFinal lstAcum =  (lstFinal++[(insertBlanks (n-largoLstAcm+numEspacios) lstAcum)])
+    where largoLstAcm = (lineLength lstAcum)  
+          numEspacios = (length lstAcum)-1
+    --el largo de la lista acumulada + el largo del siguiente token 
+
+
+separadoSinAjuste n lstTokens lstFinal = lstFinal
+
+separadoConAjuste n lstTokens lstFinal = lstFinal
